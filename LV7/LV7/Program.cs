7e8a5ca92d116b0df2953aa78df5f35def0515bb
Andrija Dujak <and123dk@gmail.com>
2020-05-30 14:28:05 +0200

    Zadatak 1

---
+++ LV7/LV7/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LV7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LV7/Palindrom/Palindromchecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    public class PalindromChecker
+    {
+        public bool check(string sentence)
+        {
+            sentence.Replace(" ", "");
+            sentence.ToLower();
+            char[] sentencearray = sentence.Where(letter => (char.IsLetterOrDigit(letter))).ToArray();
+            sentence = new string(sentencearray);
+            Array.Reverse(sentencearray);
+            string reversentence= new string(sentencearray);
+
+            return sentence.Equals(reversentence);
+        }
+
+    }
+}
