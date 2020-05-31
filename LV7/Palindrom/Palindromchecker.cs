975f19b469f173e118ec03311134caf678e4f273
Andrija Dujak <and123dk@gmail.com>
2020-05-31 09:42:35 +0200

    zadatak 3 update

--- LV7/Palindrom/Palindromchecker.cs
+++ LV7/Palindrom/Palindromchecker.cs
@@ -10,12 +10,16 @@ namespace Palindrom
     {
         public bool check(string sentence)
         {
+            sentence = makereadyforcheck(sentence);
+            return sentence.SequenceEqual(sentence.Reverse());
+        }
+        public string makereadyforcheck(string sentence)
+        {
             sentence = sentence.ToLower();
             sentence = sentence.Replace(" ", "");
             char[] sentencearray = sentence.Where(letter => (char.IsLetter(letter))).ToArray();
             sentence = new string(sentencearray);
-            return sentence.SequenceEqual(sentence.Reverse());
+            return sentence;
         }
-
     }
 }
